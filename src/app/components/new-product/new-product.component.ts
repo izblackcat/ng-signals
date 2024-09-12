5585e5f4b081f7664892a4ab3ef48bf9475480d7
izarkaoui <imzarkaoui@gmail.com>
2024-09-12 16:19:17 +0100

    update components

--- src/app/components/new-product/new-product.component.ts
+++ src/app/components/new-product/new-product.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit, signal } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Product } from '../../model/product.model';
 import { ProductService } from '../../services/product.service';
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-new-product',
   standalone: true,
-  imports: [NgClass, ReactiveFormsModule],
+  imports: [NgClass, ReactiveFormsModule, ButtonComponent],
   templateUrl: './new-product.component.html',
   styleUrl: './new-product.component.css'
 })
